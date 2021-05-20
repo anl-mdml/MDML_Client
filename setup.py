a496e345d688391ca4590e2fca54c0aee9902795
jelias1 <jelias1@luc.edu>
2021-05-20 11:17:49 -0500

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = "mdml_client",
-    version = "1.1.69",
+    version = "1.1.73",
     author = "Jakob Elias",
     author_email = "jelias@anl.gov",
     description = "Client to connect to the MDML",
