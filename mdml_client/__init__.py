acc68639094f50816296f98254a48c363d683c2a
jelias1 <jelias1@luc.edu>
2021-07-26 10:38:15 -0500

    added default schemas to client

--- mdml_client/__init__.py
+++ mdml_client/__init__.py
@@ -1,3 +1,41 @@
 from .MDML_client import *
 name = "MDML_Client"
-__version__ = "1.1.49"
+__version__ = "1.1.88"
+multipart_schema = {
+    "$schema": "http://merf.egs.anl.gov/mdml-multipart-message-schema#",
+    "title": "MultipartMessageSchema",
+    "description": "Schema for Kafka MDML data messages that are split into multiple messages",
+    "type": "object",
+    "properties": {
+        "chunk": {
+            "description": "File chunk",
+            "type": "string"
+        },
+        "part": {
+            "description": "Message part description",
+            "type": "string"
+        },
+        "filename": {
+            "description": "Name of the file",
+            "type": "string"
+        },
+        "encoding": {
+            "description": "Encoding used",
+            "type": "string"
+        }
+    },
+    "required": [ "chunk", "part", "filename", "encoding" ]
+}
+stop_funcx_schema = {
+    "$schema": "http://merf.egs.anl.gov/mdml-example-stop-funcx-schema#",
+    "title": "StopFuncx",
+    "description": "Schema for the message that will stop a FuncX function",
+    "type": "object",
+    "properties": {
+        "stop": {
+            "description": "ID of the data point",
+            "type": "boolean"
+        }
+    },
+    "required": [ "stop" ]
+}
