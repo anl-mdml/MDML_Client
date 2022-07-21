f48512beece2000e5ac1430541c920f4850ab1ca
jelias1 <jelias1@luc.edu>
2022-07-21 12:05:26 -0500

    Update test_mdml_client.py

--- tests/test_mdml_client.py
+++ tests/test_mdml_client.py
@@ -1,4 +1,38 @@
+import time
 import mdml_client as mdml
+from random import randrange
 
-mdml.kafka_mdml_producer(
-  "
+data_schema = mdml.create_schema({
+  "time": time.time(),
+  "int1": 1,
+  "int2": 2
+}, "Test schema", "Schema used for testing the MDML in GitHub Actions")
+
+producer = mdml.kafka_mdml_producer(
+  topic = "mdml-test-github-actions",
+  schema = data_schema,
+  kafka_host = "broker",
+  kafka_port = 9092,
+  schema_host = "schema-registry",
+  schema_port = 8081
+)
+
+for _ in range(5):
+  producer.produce({
+    "time": time.time(),
+    "int1": randrange(100),
+    "int2": randrange(100)
+  })
+  time.sleep(1)
+  producer.flush()
+
+consumer = mdml.kafka_mdml_consumer(
+  topics = ["mdml-test-github-actions"],
+  group = "github_actions",
+  kafka_host = "broker",
+  kafka_port = 9092,
+  schema_host = "schema-registry",
+  schema_port = 8081
+)
+for msg in consumer.consume(overall_timeout=120):
+  print(msg)
