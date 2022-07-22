0c1c81fdee2d5d01274d7bf62daba39a4dbce76d
jelias1 <jelias1@luc.edu>
2022-07-22 13:50:00 -0500

    adding prod/consumer tests

--- tests/test_mdml_client.py
+++ tests/test_mdml_client.py
@@ -10,31 +10,40 @@ def test_create_schema():
   }, "Test schema", "Schema used for testing the MDML in GitHub Actions")
   assert type(data_schema) == dict
 
-# producer = mdml.kafka_mdml_producer(
-#   topic = "mdml-test-github-actions",
-#   schema = data_schema,
-#   kafka_host = "broker",
-#   kafka_port = 9092,
-#   schema_host = "schema-registry",
-#   schema_port = 8081
-# )
+def test_kafka_mdml_producer():
+  data_schema = mdml.create_schema({
+    "time": time.time(),
+    "int1": 1,
+    "int2": 2
+  }, "Test schema", "Schema used for testing the MDML in GitHub Actions")
+  producer = mdml.kafka_mdml_producer(
+    topic = "mdml-test-github-actions",
+    schema = data_schema,
+    kafka_host = "broker",
+    kafka_port = 9092,
+    schema_host = "schema-registry",
+    schema_port = 8081
+  )
 
-# for _ in range(5):
-#   producer.produce({
-#     "time": time.time(),
-#     "int1": randrange(100),
-#     "int2": randrange(100)
-#   })
-#   time.sleep(1)
-#   producer.flush()
+  for _ in range(5):
+    producer.produce({
+      "time": time.time(),
+      "int1": randrange(100),
+      "int2": randrange(100)
+    })
+    time.sleep(1)
+    producer.flush()
 
-# consumer = mdml.kafka_mdml_consumer(
-#   topics = ["mdml-test-github-actions"],
-#   group = "github_actions",
-#   kafka_host = "broker",
-#   kafka_port = 9092,
-#   schema_host = "schema-registry",
-#   schema_port = 8081
-# )
-# for msg in consumer.consume(overall_timeout=120):
-#   print(msg)
+def test_kafka_mdml_consumer()
+  consumer = mdml.kafka_mdml_consumer(
+    topics = ["mdml-test-github-actions"],
+    group = "github_actions",
+    kafka_host = "broker",
+    kafka_port = 9092,
+    schema_host = "schema-registry",
+    schema_port = 8081
+  )
+  msgs = []
+  for msg in consumer.consume(overall_timeout=30):
+    msgs.append(msg)
+  assert len(msgs) == 5
